9d1d5fface161cba5c52e206e861cea379027bc5
SaraFormentini <127684525+SaraFormentini@users.noreply.github.com>
2023-05-01 18:45:07 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int lista[10];
+
+    for(int i=0;i<10;i++){
+       cin >> lista[i];
+    }
+    cout<<endl;
+    int a=0;
+    cin >> a;
+
+    int b=0;
+    for (int i=0;i<10;i++){
+        if(lista[i]>a){
+        b=lista[i];
+        lista[i]=a;
+        a=b;}}
+
+    for(int i=0;i<10;i++){
+        cout << lista[i] << " ";
+    }
+    return 0;
 }
